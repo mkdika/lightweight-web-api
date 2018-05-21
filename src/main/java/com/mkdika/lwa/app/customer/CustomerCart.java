f60af788a2d2fc7771eca145e91b7c2d753fca7f
Maikel Chandika <mkdika@gmail.com>
2018-05-21 09:35:24 +0700

    add class and clean project structure

---
+++ src/main/java/com/mkdika/lwa/app/customer/CustomerCart.java
@@ -0,0 +1,65 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Maikel Chandika <mkdika@gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.mkdika.lwa.app.customer;
+
+import com.mkdika.lwa.app.item.Item;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author Maikel Chandika (mkdika@gmail.com)
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class CustomerCart implements Serializable {
+    
+    @Id
+    long id;
+    
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "customer",referencedColumnName = "id")
+    Customer customer;
+    
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "item",referencedColumnName = "id")            
+    Item item;
+    
+    double qty;
+
+    public CustomerCart(Customer customer, Item item, double qty) {
+        this.customer = customer;
+        this.item = item;
+        this.qty = qty;
+    }            
+}
